ca6bbc312f7716ba2552e0aaf0e0b20aaec5aeab
Marco Colombo <ing.marco.colombo@gmail.com>
2025-10-27 14:55:45 +0100

    tests

---
+++ tests/arduino/app_bricks/sound_generator/test_abc.py
@@ -0,0 +1,95 @@
+from arduino.app_bricks.sound_generator import ABCNotationLoader
+
+
+def test_abc_loader():
+    full_abc = """
+    X:1
+    T:Main Theme
+    M:4/4
+    L:1/8
+    Q:1/4=130
+    K:Cm
+    "Cm"E2 E2 E2 "Ab"C>G | "Cm"E2 "Ab"C>G "Cm"E4 |
+    "Cm"B2 B2 B2 "Ab"c>G | "Fm"^D#2 "Ab"C>G "Cm"E4 |
+    """
+
+    reference_notes = [
+        ("E4", 60 / 130),
+        ("E4", 60 / 130),
+        ("E4", 60 / 130),
+        ("C4", (60 / 130) / 2),
+        ("G4", (60 / 130) / 2),
+        ("E4", 60 / 130),
+        ("C4", (60 / 130) / 2),
+        ("G4", (60 / 130) / 2),
+        ("E4", (60 / 130) * 2),
+        ("B4", 60 / 130),
+        ("B4", 60 / 130),
+        ("B4", 60 / 130),
+        ("C5", (60 / 130) / 2),
+        ("G4", (60 / 130) / 2),
+        ("D#4", 60 / 130),
+        ("C4", (60 / 130) / 2),
+        ("G4", (60 / 130) / 2),
+        ("E4", (60 / 130) * 2),
+    ]
+
+    metadata, loaded = ABCNotationLoader.parse_abc_notation(full_abc)
+    assert metadata["title"] == "Main Theme"
+    assert "transpose" not in metadata
+    assert metadata["tempo"] == "1/4=130"
+
+    i_ref = 0
+    for note, duration in loaded:
+        print(f"Note: {note}, Duration: {duration}")
+        assert note == reference_notes[i_ref][0]
+        assert abs(duration - reference_notes[i_ref][1]) < 0.01
+        i_ref += 1
+
+
+def test_abc_loader_with_transpose():
+    full_abc = """
+    X:1
+    T:Main Theme
+    M:4/4
+    L:1/8
+    Q:1/4=130
+    K:Cm
+    %%transpose -12
+    "Cm"E2 E2 E2 "Ab"C>G | "Cm"E2 "Ab"C>G "Cm"E4 |
+    "Cm"B2 B2 B2 "Ab"c>G | "Fm"^D#2 "Ab"C>G "Cm"E4 |
+    """
+
+    reference_notes = [
+        ("E3", 60 / 130),
+        ("E3", 60 / 130),
+        ("E3", 60 / 130),
+        ("C3", (60 / 130) / 2),
+        ("G3", (60 / 130) / 2),
+        ("E3", 60 / 130),
+        ("C3", (60 / 130) / 2),
+        ("G3", (60 / 130) / 2),
+        ("E3", (60 / 130) * 2),
+        ("B3", 60 / 130),
+        ("B3", 60 / 130),
+        ("B3", 60 / 130),
+        ("C4", (60 / 130) / 2),
+        ("G3", (60 / 130) / 2),
+        ("D#3", 60 / 130),
+        ("C3", (60 / 130) / 2),
+        ("G3", (60 / 130) / 2),
+        ("E3", (60 / 130) * 2),
+    ]
+
+    metadata, loaded = ABCNotationLoader.parse_abc_notation(full_abc)
+    assert metadata["title"] == "Main Theme"
+    assert "transpose" in metadata
+    assert metadata["transpose"] == -1
+    assert metadata["tempo"] == "1/4=130"
+
+    i_ref = 0
+    for note, duration in loaded:
+        print(f"Note: {note}, Duration: {duration}")
+        assert note == reference_notes[i_ref][0]
+        assert abs(duration - reference_notes[i_ref][1]) < 0.01
+        i_ref += 1
---
+++ tests/arduino/app_bricks/sound_generator/test_effects.py
@@ -0,0 +1,68 @@
+# SPDX-FileCopyrightText: Copyright (C) 2025 ARDUINO SA <http://www.arduino.cc>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+import pytest
+from arduino.app_bricks.sound_generator.effects import SoundEffect
+from arduino.app_bricks.sound_generator import SoundGenerator
+from arduino.app_utils.audio import WaveGenerator
+
+
+def test_adsr_effect():
+    generator = WaveGenerator(sample_rate=16000, wave_form="square")
+    adsr = SoundEffect.adsr()
+    blk = generator.generate_block(440.0, 1 / 8, 1.0)  # Generate a block to initialize
+    assert adsr is not None
+
+    # Apply ADSR effect
+    processed = adsr.apply(blk)
+
+    assert processed is not None
+    assert len(processed) == len(blk)
+
+
+def test_available_notes():
+    note_sequence = [
+        ("E5", 0.125),
+        ("E5", 0.125),
+        ("REST", 0.125),
+        ("E5", 0.125),
+        ("REST", 0.125),
+        ("C5", 0.125),
+        ("E5", 0.125),
+        ("REST", 0.125),
+        ("G5", 0.25),
+        ("REST", 0.25),
+        ("G4", 0.25),
+        ("REST", 0.25),
+        ("C5", 0.25),
+        ("REST", 0.125),
+        ("G4", 0.25),
+        ("REST", 0.125),
+        ("E4", 0.25),
+        ("REST", 0.125),
+        ("A4", 0.25),
+        ("B4", 0.25),
+        ("Bb4", 0.125),
+        ("A4", 0.25),
+        ("G4", 0.125),
+        ("E5", 0.125),
+        ("G5", 0.125),
+        ("A5", 0.25),
+        ("F5", 0.125),
+        ("G5", 0.125),
+        ("REST", 0.125),
+        ("E5", 0.25),
+        ("C5", 0.125),
+        ("D5", 0.125),
+        ("B4", 0.25),
+    ]
+
+    generator = SoundGenerator()
+    for note, duration in note_sequence:
+        print(f"Testing note: {note}")
+        frequency = generator._get_note(note)
+        if "REST" != note:
+            assert frequency is not None and frequency > 0.0
+        else:
+            assert frequency is not None and frequency == 0.0
