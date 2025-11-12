bf6dab1ce9b8b58c67aa39b17ec80b7287603483
Marco Colombo <ing.marco.colombo@gmail.com>
2025-11-12 16:18:17 +0100

    Started docker container definition

--- containers/piper-tts/src/python/piper-server.py
+++ containers/piper-tts/src/python/piper-server.py
@@ -1,8 +1,7 @@
 from piper import PiperVoice
 from piper.config import SynthesisConfig
-from arduino.app_peripherals.speaker import Speaker
 
-voice = PiperVoice.load("en_US-lessac-low.onnx")
+voice = PiperVoice.load("/app/models/en_US-lessac-low.onnx")
 
 long_story = """
 Once upon a time, in a quiet village surrounded by green hills, there lived a boy named Alberto and his best friend, a golden dog named Milo.
@@ -11,9 +10,6 @@ One summer day, a storm rolled in unexpectedly. The wind howled, and the rain fe
 When they reached the river, the kite was tangled in a bush dangerously close to the water. As Alberto tried to pull it free, his foot slipped on the muddy ground—and he tumbled in. The current was strong and cold. He cried out, but the wind swallowed his voice.
 """
 
-out = Speaker(sample_rate=16000, format="FLOAT_LE")
-out.start()
-
 cfg = SynthesisConfig(
     length_scale=1.4,
 )
@@ -21,7 +17,6 @@ cfg = SynthesisConfig(
 print("---> Start streaming")
 for chunk in voice.synthesize(text=long_story, syn_config=cfg):
     print(f"[C]--->{chunk}")
-    out.play(chunk.audio_float_array, block_on_queue=True)
 
 import time
 
