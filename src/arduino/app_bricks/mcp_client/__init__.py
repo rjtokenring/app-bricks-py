4ea47001aa2f6b1b8acbbad82f358daee97fb04c
Marco Colombo <ing.marco.colombo@gmail.com>
2026-02-13 14:50:47 +0100

    Refactoring

--- src/arduino/app_bricks/mcp_client/__init__.py
+++ src/arduino/app_bricks/mcp_client/__init__.py
@@ -2,27 +2,80 @@
 #
 # SPDX-License-Identifier: MPL-2.0
 
-from langchain_mcp_adapters.client import MultiServerMCPClient
 import asyncio
+from abc import ABC, abstractmethod
+from langchain_mcp_adapters.client import MultiServerMCPClient
+
 from arduino.app_utils import brick
+
+
+class MCPEndpoint(ABC):
+    """A class representing an MCP endpoint configuration."""
+
+    def __init__(self, name: str, transport: str, **kwargs):
+        self.name = name
+        self.transport = transport
+        self.config = kwargs
+
+    @abstractmethod
+    def to_dict(self):
+        config: dict = {}
+        config[self.name] = {
+            "transport": self.transport,
+            **self.config,
+        }
+        return config
+
+
+class HTTPEndpoint(MCPEndpoint):
+    """A class to communicate with remote MCP server via HTTP protocol to perform various tasks."""
+
+    def __init__(self, name: str, url: str, headers: dict = None):
+        super().__init__(name=name, transport="http", url=url, headers=headers)
+
+    def to_dict(self):
+        return super().to_dict()
+
+
+class LocalPythonMCPEndpoint(MCPEndpoint):
+    """A class to communicate with a local Python MCP server to perform various tasks."""
+
+    def __init__(self, name: str, script_path: str, args: list = None):
+        super().__init__(name=name, transport="stdio", command="python", args=[script_path] + (args or []))
+
+    def to_dict(self):
+        return super().to_dict()
 
 
 @brick
 class MCPClient:
     """A class to communicate with the MCP server to perform various tasks."""
 
-    def __init__(self, url: str = "http://localhost:8000/mcp", transport: str = "streamable_http", tool_name_prefix: bool = False, **kwargs):
-        """Initialize the MCPClient with a MultiServerMCPClient."""
+    def __init__(self, clients: list[MCPEndpoint], tool_name_prefix: bool = True, **kwargs):
+        """Initialize the MCPClient with a MultiServerMCPClient.
+        
+        Args:
+            clients (list[MCPEndpoint]): A list of MCP endpoint configurations. Use brick's exposed endpoint classes like HTTPEndpoint or LocalPythonMCPEndpoint to create endpoint configurations.
+            tool_name_prefix (bool, optional): Whether to prefix tool names with the client name. Defaults to True.
+            **kwargs: Additional keyword arguments to pass to the MultiServerMCPClient.
+        
+        """
+        connections = {}
+        for client in clients:
+            connections.update(client.to_dict())
         self._client = MultiServerMCPClient(
-            connections={
-                "weather": {
-                    "transport": transport,
-                    "url": url,
-                }
-            },
+            connections=connections,
             tool_name_prefix=tool_name_prefix,
             **kwargs,
         )
+
+    def get_client(self) -> MultiServerMCPClient:
+        """Get the underlying MultiServerMCPClient instance.
+
+        Returns:
+            MultiServerMCPClient: The underlying MCP client instance.
+        """
+        return self._client
 
     def list_tools(self) -> str:
         """List the available tools from the MCP server.
---
+++ src/arduino/app_bricks/mcp_client/examples/1_simple_mcp_client.py
@@ -0,0 +1,16 @@
+# SPDX-FileCopyrightText: Copyright (C) ARDUINO SRL (http://www.arduino.cc)
+#
+# SPDX-License-Identifier: MPL-2.0
+
+# EXAMPLE_NAME = "Basic mcp client usage example"
+
+from arduino.app_bricks.mcp_client import MCPClient, LocalPythonMCPEndpoint
+from arduino.app_utils import App
+
+local = LocalPythonMCPEndpoint(name="local_mcp", script_path="math_server.py")
+
+client = MCPClient(clients=[local])
+
+print(client.list_tools())
+
+App.run()
